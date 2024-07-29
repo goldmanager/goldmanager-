1a58d66e9336352ae2b7613a7d8cc209008f6ade
fg12111 <github@02n.net>
2024-07-30 01:11:29 +0200

    fix api url

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL+"/api/",
+  baseURL: (process.env.VUE_APP_API_BASE_URL ?? "") +"/api/",
 });
 
 
