5e93d9f95e0bf76421e81e5ea11329b5f4f7a997
fg12111 <github@02n.net>
2024-07-31 01:01:30 +0200

    use only cookies for csrf

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,29 +1,7 @@
 import axios from 'axios';
 
-function getCsrfToken() {
-
-	if (typeof document === 'undefined') {
-		return null;
-	}
-
-	const cookies = document.cookie;
-
-	if (!cookies) {
-		return null;
-	}
-
-	const csrfToken = cookies.split('; ').find(row => row.startsWith('XSRF-TOKEN='));
-	return csrfToken ? csrfToken.split('=')[1] : null;
-
-}
-
-const csrfToken = getCsrfToken();
-
 const instance = axios.create({
 	baseURL: (process.env.VUE_APP_API_BASE_URL ?? "") + "/api/",
-	headers: {
-		'X-XSRF-TOKEN': csrfToken
-	},
 	withCredentials: true
 });
 
