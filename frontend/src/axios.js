1633ef39e48599d2b200322cc894aae84dd68ad4
fg12111 <github@02n.net>
2024-07-31 00:38:55 +0200

    fixed npm build

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,34 +2,40 @@ import axios from 'axios';
 
 function getCsrfToken() {
 
-	   const cookies = document.cookie;	
-	   if (!cookies) {
-	       return null; 
-	   }
+	if (typeof document === 'undefined') {
+		return null;
+	}
 
-	   const csrfToken = cookies.split('; ').find(row => row.startsWith('XSRF-TOKEN='));
-	   return csrfToken ? csrfToken.split('=')[1] : null;
+	const cookies = document.cookie;
+
+	if (!cookies) {
+		return null;
+	}
+
+	const csrfToken = cookies.split('; ').find(row => row.startsWith('XSRF-TOKEN='));
+	return csrfToken ? csrfToken.split('=')[1] : null;
+
 }
 
 const csrfToken = getCsrfToken();
 
 const instance = axios.create({
-  baseURL: (process.env.VUE_APP_API_BASE_URL ?? "") +"/api/",
-  headers: {
-         'X-XSRF-TOKEN': csrfToken
-  },
-  withCredentials: true
+	baseURL: (process.env.VUE_APP_API_BASE_URL ?? "") + "/api/",
+	headers: {
+		'X-XSRF-TOKEN': csrfToken
+	},
+	withCredentials: true
 });
 
 
 
 instance.interceptors.request.use(config => {
-  const token = sessionStorage.getItem('jwt-token');
-  if (token) {
-    config.headers['Authorization'] = `Bearer ${token}`;
-  }
- 
-  return config;
+	const token = sessionStorage.getItem('jwt-token');
+	if (token) {
+		config.headers['Authorization'] = `Bearer ${token}`;
+	}
+
+	return config;
 });
 
 
