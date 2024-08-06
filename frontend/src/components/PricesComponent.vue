11cd07667c632d2ed8a3a54a9774598e95aa3cd4
fg12111 <github@02n.net>
2024-08-06 18:18:03 +0200

    Applied Pagination & search to PriceList

--- frontend/src/components/PricesComponent.vue
+++ frontend/src/components/PricesComponent.vue
@@ -4,6 +4,7 @@
     <div><h1>Prices</h1></div>
     <div v-if="errorMessage" class="error">{{ errorMessage }}</div>
     <div v-if="currentViewType==='PriceList'">
+		<div><input v-model="searchQueryPriceList" type="text" placeholder="Search by item name"></div>
     <table>
       <thead>
         <tr>
@@ -29,21 +30,22 @@
       </tr>
       </tbody>
       </table>
-      <table v-if="priceList.prices.length >0">
+      <div  v-if="priceList.prices.length >0">
+      <table>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Weight</th>
-            <th>Unit</th>
-            <th>Number of Items</th>
-            <th>Unit Price</th>
-            <th>Total Price</th>
-            <th>Metal</th>
+            <th @click="sortPriceListBy('name')">Name <span v-if="currentPriceListSort === 'name'">{{ currentPriceListSortDir === 'asc' ? '▲' : '▼' }}</span></th>
+            <th @click="sortPriceListBy('amount')">Weight <span v-if="currentPriceListSort === 'amount'">{{ currentPriceListSortDir === 'asc' ? '▲' : '▼' }}</span></th>
+            <th @click="sortPriceListBy('unit')">Unit <span v-if="currentPriceListSort === 'unit'">{{ currentPriceListSortDir === 'asc' ? '▲' : '▼' }}</span></th>
+            <th @click="sortPriceListBy('itemCount')">Number of Items <span v-if="currentPriceListSort === 'itemCount'">{{ currentPriceListSortDir === 'asc' ? '▲' : '▼' }}</span></th>
+            <th @click="sortPriceListBy('price')">Unit Price <span v-if="currentPriceListSort === 'price'">{{ currentPriceListSortDir === 'asc' ? '▲' : '▼' }}</span></th>
+            <th @click="sortPriceListBy('priceTotal')">Total Price <span v-if="currentPriceListSort === 'priceTotal'">{{ currentPriceListSortDir === 'asc' ? '▲' : '▼' }}</span></th>
+            <th @click="sortPriceListBy('metal')">Metal <span v-if="currentPriceListSort === 'metal'">{{ currentPriceListSortDir === 'asc' ? '▲' : '▼' }}</span></th>
           </tr>
         </thead>
         <tbody>
 
-          <tr v-for="price in priceList.prices" :key="price.item.id">
+          <tr v-for="price in paginatedPriceList.prices" :key="price.item.id">
             <td>{{ price.item.name }}</td>
             <td>{{ price.item.amount }}</td>
             <td>{{ price.item.unit.name }}</td>
@@ -54,7 +56,14 @@
           </tr>
         </tbody>
       </table>
+      <div class="pagination" v-if="totalPriceListPages > 0">
+		<button :class="currentPriceListPage === 1 ?'pagingButton_disabled':'pagingButton'" @click="prevPriceListPage" :disabled="currentPriceListPage === 1">Previous</button>
+		<span>Page {{ currentPriceListPage }} of {{ totalPriceListPages }}</span>
+		<button  :class="currentPriceListPage === totalPriceListPages?'pagingButton_disabled':'pagingButton'" @click="nextPriceListPage" :disabled="currentPriceListPage === totalPriceListPages">Next</button>
+		<span>(Items per page: {{priceListPageSize}})</span>		 
+	</div>
     </div>
+	</div>
    <div v-else >
     <table>
       <thead>
@@ -125,8 +134,8 @@
 </template>
 
 <script>
+/*eslint no-mixed-spaces-and-tabs: ["error", "smart-tabs"]*/
 import axios from '../axios';
-
 export default {
   name: 'PricesComponent',
   data() {
@@ -148,7 +157,12 @@ export default {
       metals: [],
       currentViewType: 'PriceList',
       errorMessage: '',
-      currentFilter: ''
+      currentFilter: '',
+	  searchQueryPriceList:'',
+	  priceListPageSize:10,
+	  currentPricePageNumber: 1,
+	  currentPriceListSortDir:'',
+	  currentPriceListSort:'name'
     };
 
   },
@@ -156,9 +170,80 @@ export default {
   mounted() {
     this.currentFilter=this.getCurrentFilter();
     this.currentViewType=this.getCurrentViewType();
+	this.currentPriceListSort=localStorage.getItem("PriceListColumnsSort")?localStorage.getItem("PriceListColumnsSort"):"name";
+	this.currentPriceListSortDir = localStorage.getItem("PriceListColumnsSortDir")?localStorage.getItem("PriceListColumnsSortDir"):"asc";
     this.fetchData();
 
   },
+  computed: {
+	sortedPriceList() {
+		
+		let sortedPrices =[...this.priceList.prices];
+		return sortedPrices.sort((a, b) => {
+			let modifier = 1;
+			if (this.currentPriceListSortDir === 'desc') modifier = -1;
+			let valA = "";
+			let valB = "";
+			if (this.currentPriceListSort === 'name') {
+				valA = a.item.name;
+				valB = b.item.name;
+			}
+			else if (this.currentPriceListSort === 'amount') {
+				valA = a.item.amount;
+				valB = b.item.amount;
+			}
+			else if (this.currentPriceListSort === 'unit') {
+				valA = a.item.unit.name;
+				valB = b.item.unit.name;
+			}
+			else if (this.currentPriceListSort === 'itemCount') {
+				valA = a.item.itemCount;
+				valB = b.item.itemCount;
+			}
+			else if (this.currentPriceListSort === 'metal') {
+				valA = a.item.itemType.material.name;
+				valB = b.item.itemType.material.name;
+			}							
+			else {
+				valA = a[this.currentPriceListSort];
+				valB = b[this.currentPriceListSort];
+			}
+			if (typeof valA === 'string' && typeof valB === 'string') {
+				valA = valA.toLowerCase();
+				valB = valB.toLowerCase();
+			}
+			if (valA < valB) return -1 * modifier;
+			if (valA > valB) return 1 * modifier;
+			return 0;
+		});
+		
+	},
+	filteredPriceList() {
+		if (this.searchQueryPriceList != null && this.searchQueryPriceList != '') {		
+			return this.sortedPriceList.filter(price =>
+			price.item.name.toLowerCase().includes(this.searchQueryPriceList.toLowerCase())
+			);
+		}
+		return this.sortedPriceList;
+	},
+	paginatedPriceList() {
+		let result = {};
+		result["totalPrice"] = this.priceList.totalPrice;
+		let start = (this.currentPriceListPage - 1) * this.priceListPageSize;
+		let end = start + this.priceListPageSize;
+		result["prices"]= this.filteredPriceList.slice(start, end);
+		return result;
+	},
+	totalPriceListPages() {
+		return Math.ceil(this.filteredPriceList.length / this.priceListPageSize);
+	},
+	currentPriceListPage() {
+		if(this.currentPricePageNumber > this.totalPriceListPages){
+			return 1;
+		}
+		return this.currentPricePageNumber;
+	},
+},
   methods: {
     getCurrentFilter(){
       return localStorage.getItem("PriceMaterialFilter")?localStorage.getItem("PriceMaterialFilter"):"";
@@ -189,6 +274,7 @@ export default {
       if(this.currentViewType === "PriceList"){
          console.log("PriceList");
          this.fetchPriceList();
+		 this.currentPricePageNumber=1;
       }
       else{
         this.fetchPriceGroupsByMetal();
@@ -258,8 +344,28 @@ export default {
           else{
             this.errorMessage=defaultMessage;
           }
-    }
-
+    },
+	sortPriceListBy(column){			
+		if (this.currentPriceListSort === column) {
+			this.currentPriceListSortDir = this.currentPriceListSortDir === 'asc' ? 'desc' : 'asc';
+			localStorage.setItem("PriceListColumnsSortDir",this.currentSortDir )				
+		}
+		this.currentPriceListSort=column;
+		localStorage.setItem("PriceListColumnsSort",column);
+		
+	},
+	nextPriceListPage() {
+		if (this.currentPriceListPage < this.totalPriceListPages) {
+			this.currentPricePageNumber = this.currentPriceListPage+1;
+		}
+	},
+	prevPriceListPage() {
+		
+		if (this.currentPriceListPage > 1) {
+			this.currentPricePageNumber = this.currentPriceListPage-1;
+		}
+		
+	}		
   }
 };
 </script>
