3240b602679a0a157215921e681175eeab31bac8
fg12111 <github@02n.net>
2025-01-04 20:17:24 +0100

    Fix build

--- backend/src/test/java/com/my/goldmanager/entity/TestData.java
+++ backend/src/test/java/com/my/goldmanager/entity/TestData.java
@@ -2,36 +2,76 @@ package com.my.goldmanager.entity;
 
 import java.util.List;
 
-import lombok.Getter;
-import lombok.Setter;
-
 public class TestData {
 
-	@Getter
-	@Setter
 	private List<Unit> units;
 
-	@Getter
-	@Setter
 	private List<Material> metals;
 
-	@Getter
-	@Setter
 	private List<UserLogin> users;
 
-	@Getter
-	@Setter
 	private List<ItemStorage> itemStorages;
 
-	@Getter
-	@Setter
 	private List<MaterialHistory> materialHistories;
 
-	@Getter
-	@Setter
 	private List<ItemType> itemTypes;
 
-	@Getter
-	@Setter
 	private List<Item> items;
+
+	public List<Unit> getUnits() {
+		return units;
+	}
+
+	public void setUnits(List<Unit> units) {
+		this.units = units;
+	}
+
+	public List<Material> getMetals() {
+		return metals;
+	}
+
+	public void setMetals(List<Material> metals) {
+		this.metals = metals;
+	}
+
+	public List<UserLogin> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<UserLogin> users) {
+		this.users = users;
+	}
+
+	public List<ItemStorage> getItemStorages() {
+		return itemStorages;
+	}
+
+	public void setItemStorages(List<ItemStorage> itemStorages) {
+		this.itemStorages = itemStorages;
+	}
+
+	public List<MaterialHistory> getMaterialHistories() {
+		return materialHistories;
+	}
+
+	public void setMaterialHistories(List<MaterialHistory> materialHistories) {
+		this.materialHistories = materialHistories;
+	}
+
+	public List<ItemType> getItemTypes() {
+		return itemTypes;
+	}
+
+	public void setItemTypes(List<ItemType> itemTypes) {
+		this.itemTypes = itemTypes;
+	}
+
+	public List<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+
 }
